71c38cc5e740b21ec7e318c50b250b089a00e3a2
kyuwonism@gmail.com <kyuwonism@gmail.com>
2023-01-10 00:30:04 +0900

    add axios

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    News..
+    <div v-for="user in users" v-bind:key="user">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
+import { fetchNewsList } from '../api/index.js';
 export default {
-
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    fetchNewsList()
+    .then(response => {
+      console.log(response);
+      this.users = response.data;
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
 }
 </script>
 
