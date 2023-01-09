40ef45db76d6f031bd7f58c4c4661e5b309a0b49
kyuwonism <kimroot@kakao.com>
2023-01-09 21:17:03 +0900

    vue-new app first upload

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <toolbar-area></toolbar-area>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToolbarArea from './components/ToolbarArea.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ToolbarArea
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  padding: 0;
+  margin: 0;
+}
+a {
+  text-decoration: none;
 }
 </style>
---
+++ src/routes/index.js
@@ -0,0 +1,40 @@
+import { createWebHistory, createRouter} from 'vue-router'
+import NewsView from '../views/NewsView.vue'
+import AskView from '../views/AskView.vue'
+import JobsView from '../views/JobsView.vue'
+import ItemView from '../views/ItemView.vue'
+import UserView from '../views/UserView.vue'
+
+const routes = [
+  {
+    paht: '/',
+    redirect: '/news',
+  },
+  {
+    path: '/news',
+    component: NewsView, 
+  },
+  {
+    path: '/ask',
+    component: AskView,
+  },
+  {
+    path: '/jobs',
+    component: JobsView,
+  },
+  {
+    path: '/item',
+    component: ItemView,
+  },
+  {
+    path: '/user',
+    component: UserView,
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
